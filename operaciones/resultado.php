4ac7b6585095fe822c22470dc5a750fc4cac461c
pelayo arellano jorge oswaldo <valdo_jopa@hotmail.com>
2015-10-25 14:23:53 -0600

    operaciones simples en html y php

---
+++ operaciones/resultado.php
@@ -0,0 +1,18 @@
+<html>
+<heat></head>
+<body>
+<?php
+
+$suma=$num1+$num2;
+$resta=$num1-$num2;
+$multi=$num1*$num2;
+$divi=$num1/$num2;
+
+echo "la suma es: $suma </br>";
+echo "la resta es: $resta </br>";
+echo "la multiplicacion es: $multi </br>";
+echo "la division es: $divi </br>";
+
+?>
+</body>
+</html>
